c50cc324c39af5d3c4f9968f09d38468a06f3ac6
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2020-05-15 08:30:25 +0000

    first commit

--- nginx/scripts/build-nginx.sh
+++ nginx/scripts/build-nginx.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-for conf in /etc/nginx/templates/*.conf; do
-    mv $conf "/etc/nginx/sites-available/"$(basename $conf) > /dev/null
-done
+#for conf in /etc/nginx/templates/*.conf; do
+#    mv $conf "/etc/nginx/sites-available/"$(basename $conf) > /dev/null
+#done
 
-for template in /etc/nginx/templates/*.template; do
-    envsubst < $template > "/etc/nginx/sites-available/"$(basename $template)".conf"
-done
+#for template in /etc/nginx/templates/*.template; do
+#    envsubst < $template > "/etc/nginx/sites-available/"$(basename $template)".conf"
+#done
 
 if [[ "$NO_DEFAULT" = true ]]; then
-    rm /etc/nginx/sites-available/node.template.conf
-    rm /etc/nginx/sites-available/node-https.template.conf
+     echo "oke"
+#    rm /etc/nginx/sites-available/node.template.conf
+#    rm /etc/nginx/sites-available/node-https.template.conf
 else
     if [[ "$WEB_SSL" = false ]]; then
         rm /etc/nginx/sites-available/node-https.template.conf
